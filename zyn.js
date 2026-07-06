182d36504573067236e3055fe918bfe293983d1b
⊱: ̗̀SσყMαყƈσʟ࿐ <karatekidamericatv@gmail.com>
2026-07-05 21:29:29 -0500

    Update zyn.js

--- zyn.js
+++ zyn.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { main } = require('./src/cli/runtime');
-main().catch(err => {
-  console.error(`Error: ${err.message}`);
+main().catch((err) => {
+  console.error(err.message);
   process.exit(1);
 });
